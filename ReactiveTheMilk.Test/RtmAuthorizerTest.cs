560b5062fd284b8a58a9c085464c19d8ec8d7a10
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail>
2011-11-24 23:14:14 +0900

    rtm.auth.getFrobの実装
    ただし、Webアクセス部分は別メソッドに切りだしたため、未実装
    rspからfrobを取り出す部分のみ

--- ReactiveTheMilk.Test/RtmAuthorizerTest.cs
+++ ReactiveTheMilk.Test/RtmAuthorizerTest.cs
@@ -3,33 +3,66 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reactive.Linq;
+using System.Net;
+using ReactiveTheMilk.Moles;
+using System.Net.Moles;
+
 
 namespace ReactiveTheMilk
 {
-  [TestClass]
-  public class RtmAuthorizerTest
-  {
-    private const string ApiKey = "api_key";
-    private const string Secret = "signature";
-    private const string Frob = "frob";
+	[TestClass]
+	public class RtmAuthorizerTest
+	{
+		private const string ApiKey = "api_key";
+		private const string Secret = "signature";
+		private const string Frob = "frob";
 
-    private RtmAuthorizer rtm;
+		private SRtmAuthorizer rtm;
 
-    [TestInitialize]
-    public void SetUp()
-    {
-      rtm = new RtmAuthorizer(ApiKey, Secret);
-    }
+		[TestInitialize]
+		public void SetUp()
+		{
+			rtm = new SRtmAuthorizer(ApiKey, Secret);
+		}
 
-    [TestMethod()]
-    public void TestGenerateSignature()
-    {
-      var method = new Parameter("method", "rtm");
-      var api_key = new Parameter("api_key", ApiKey);
-      var parameters = new Parameter[] { method, api_key };
-      String signature = rtm.GenerateSignature(parameters);
+		[TestMethod]
+		public void TestGenerateSignature()
+		{
+			var method = new Parameter("method", "rtm");
+			var api_key = new Parameter("api_key", ApiKey);
+			var parameters = new Parameter[] { method, api_key };
+			String signature = rtm.GenerateSignature(parameters);
 
-      Assert.AreEqual(signature, "2210109e7b5762b07190d4fb5abc68c7");
-    }
-  }
+			signature.Is("2210109e7b5762b07190d4fb5abc68c7");
+		}
+
+		[TestMethod]
+		[HostType("Moles")]
+		public void TestGetFrob()
+		{
+			var rspRaw = @"<rsp stat=""ok""><frob>frob</frob></rsp>";
+
+			MRtmAuthorizer.AllInstances.GetRtmResponseString = (a, method) =>
+			{
+				method.Is("rtm.auth.getFrob");
+				return Observable.Return(rspRaw);
+			};
+
+			IObservable<string> result = rtm.GetFrob();
+			string frob = result.First();
+
+			frob.Is("frob");
+		}
+
+		[TestMethod]
+		public void TestCreateRtmWebRequest()
+		{
+			WebRequest request = RtmAuthorizer.CreateRtmWebRequest();
+
+			request.RequestUri.OriginalString.Is("https://api.rememberthemilk.com/services/rest/");
+			request.Method.Is("POST");
+			request.ContentType.Is("application/x-www-form-urlencoded");
+		}
+	}
 }
--- ReactiveTheMilk/RtmAuthorizer.cs
+++ ReactiveTheMilk/RtmAuthorizer.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reactive.Linq;
+using System.Net;
+using System.Xml.Linq;
 
 namespace ReactiveTheMilk
 {
@@ -13,5 +16,26 @@ namespace ReactiveTheMilk
     public RtmAuthorizer(string apiKey, string secret) : base(apiKey, secret)
     {
     }
-  }
+
+		public IObservable<string> GetFrob()
+		{
+			IObservable<string> result = GetRtmResponse("rtm.auth.getFrob");
+			return result
+				.Select(rspRaw => XElement.Parse(rspRaw))
+				.Select(rsp => rsp.Element("frob").Value);
+		}
+
+		public IObservable<string> GetRtmResponse(string method)
+		{
+			return null;
+		}
+
+		public static WebRequest CreateRtmWebRequest()
+		{
+			WebRequest request = WebRequest.Create("https://api.rememberthemilk.com/services/rest/");
+			request.Method = "POST";
+			request.ContentType = "application/x-www-form-urlencoded";
+			return request;
+		}
+	}
 }
