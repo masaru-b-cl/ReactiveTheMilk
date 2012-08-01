56f903a120263201e7d0a1acc793f9e48b5139aa
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2012-08-02 00:41:52 +0900

    簡易サンプル作成：コンソールアプリでリスト一覧表示

---
+++ ReactiveTheMilk.ConsoleApplication/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace ReactiveTheMilk.ConsoleApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string apiKey = "(api-key)";
+      string secret = "(secret)";
+      var rtmAuthorizer = new RtmAuthorizer(apiKey, secret);
+
+      string frob = rtmAuthorizer.GetFrob().First();
+
+      string authenticationUrl = rtmAuthorizer.GetAuthenticationUrl(frob);
+
+      //Process.Start(authenticationUrl);
+
+      //Console.WriteLine("Webページで認証してください。...");
+
+      //Console.ReadKey();
+
+      //var token = rtmAuthorizer.GetToken(frob).First();
+
+      //Console.WriteLine("Token:{0}", token.Token);
+
+      RtmClient client = new RtmClient(apiKey, secret, "(token)");
+
+      var lists = client.GetLists();
+
+      var list = lists.First();
+
+    }
+  }
+}
