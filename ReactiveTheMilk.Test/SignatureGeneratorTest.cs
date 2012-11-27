d54a0b45974d09a3953a43b4216388c2bf9b0736
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2012-11-28 01:28:22 +0900

    signature生成を行う専用クラスを作成

---
+++ ReactiveTheMilk.Test/SignatureGeneratorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReactiveTheMilk
+{
+  [TestClass]
+  public class SignatureGeneratorTest
+  {
+    [TestMethod]
+    public void TestConstructor()
+    {
+      string secret = "secret";
+      var generator = new SignatureGenerator(secret).AsDynamic();
+
+      (generator.secret as string).Is("secret");
+    }
+
+    [TestMethod]
+    public void TestGenerateSignature()
+    {
+      var method = new Parameter("method", "rtm");
+      var api_key = new Parameter("api_key", "api_key");
+      var parameters = new Parameter[] { method, api_key };
+      var secret = "secret";
+      var generator = new SignatureGenerator(secret);
+
+      String signature = generator.Generate(parameters);
+
+      signature.Is("1770f4c43928e83b2c301ead6604464d");
+    }
+  }
+}
---
+++ ReactiveTheMilk/SignatureGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace ReactiveTheMilk
+{
+  public class SignatureGenerator
+  {
+    /// <summary>
+    /// RTM API secret
+    /// </summary>
+    private readonly string secret;
+
+    /// <summary>
+    /// ReactiveTheMilk.SignatureGenerator クラスの新しいインスタンスを初期化し、secretを設定します。
+    /// </summary>
+    /// <param name="secret">RTM API secret。</param>
+    public SignatureGenerator(string secret)
+    {
+      this.secret = secret;
+    }
+
+    /// <summary>
+    /// RTM APIパラメーターを元に、signatureを生成します。
+    /// </summary>
+    /// <param name="parameters">RTM APIパラメーター。</param>
+    /// <returns>signature。</returns>
+    public string Generate(IEnumerable<Parameter> parameters)
+    {
+      var orderedFlattenedParameters = parameters
+        .OrderBy(x => x.Key)
+        .Select(x => x.Key + x.Value)
+        .Concat();
+
+      string signatureSource = secret + orderedFlattenedParameters;
+      using (var md5 = MD5.Create())
+      {
+        byte[] md5HashedBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(signatureSource));
+        return BitConverter.ToString(md5HashedBytes).ToLower().Replace("-", "");
+      }
+    }
+  }
+}
